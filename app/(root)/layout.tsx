9eaff58b2545a1be50a5a00dba3dafdac077a421
Roshan <roshankrsu@gmail.com>
2026-05-03 20:08:28 +0530

    added landing page design

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,39 @@
+import { Footer } from "@/modules/home/footer";
+import { Header } from "@/modules/home/header";
+import { cn } from "@/lib/utils";
+import type { Metadata } from "next";
+// import { usePathname } from "next/navigation";
+
+export const metadata: Metadata = {
+  title: {
+    template: "VibeCode - Editor ",
+    default: "VibeCode IDE",
+  },
+  icons: {
+    icon: "/logo.jpeg", 
+  },
+};
+
+export default function HomeLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <Header />
+      <div
+        className={cn(
+          "absolute inset-0",
+          "bg-size:40px_40px",
+          "bg-[linear-gradient(to_right,#e4e4e7_1px,transparent_1px),linear-gradient(to_bottom,#e4e4e7_1px,transparent_1px)]",
+          "dark:bg-[linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)]",
+        )}
+      />
+      <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white mask-[radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black" />
+
+      <main className="z-20 relative w-full pt-0 md:pt-0  ">{children}</main>
+      <Footer />
+    </>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { SessionProvider } from "next-auth/react";
 import { auth } from "@/auth";
+import { ThemeProvider } from "@/components/ui/providers/theme-providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,8 +25,7 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-
-  const session = await auth()
+  const session = await auth();
 
   return (
     <SessionProvider session={session}>
@@ -33,7 +33,16 @@ export default async function RootLayout({
         lang="en"
         className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
       >
-        <body className="min-h-full flex flex-col">{children}</body>
+        <body className="min-h-full flex flex-col">
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </body>
       </html>
     </SessionProvider>
   );
