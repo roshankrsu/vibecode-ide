8c1f407ae8a88263fc446ca9274028d34b8040e7
Roshan <roshankrsu@gmail.com>
2026-05-08 17:56:05 +0530

    added get-port for autamatic avilable port assigning

--- vibecode-starters/hono-nodejs-starter/src/index.ts
+++ vibecode-starters/hono-nodejs-starter/src/index.ts
@@ -1,13 +1,15 @@
-import { serve } from '@hono/node-server';
-import { Hono } from 'hono';
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import getPort from "get-port";
 
 const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!');
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
 });
 
-const port = 3000;
+const port = await getPort();
+
 console.log(`Server is running on http://localhost:${port}`);
 
 serve({
