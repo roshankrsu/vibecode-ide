e202cf5cc11fc35806c10723dcbb6af24eac4598
Roshan <roshankrsu@gmail.com>
2026-05-08 20:09:25 +0530

    updated react template code

--- vibecode-starters/react-ts/src/index.tsx
+++ vibecode-starters/react-ts/src/index.tsx
@@ -3,7 +3,13 @@ import { createRoot } from 'react-dom/client';
 
 import { App } from './App';
 
-const root = createRoot(document.getElementById('app'));
+const container = document.getElementById("app");
+
+if (!container) {
+  throw new Error("Root container not found");
+}
+
+const root = createRoot(container);
 
 root.render(
   <StrictMode>
